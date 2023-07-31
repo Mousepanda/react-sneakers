cfb3575232cc89d002292755ad3e851d0777ae95
Mousepanda <mousepanda777@gmail.com>
2023-07-31 22:45:10 +0500

    Сделал сомнительное удаление компонента

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
 import Card from "./components/Card";
 import Header from "./components/Header";
 import Drawer from "./components/Drawer";
+import React from "react";
 
-const arr = [
-	{title: 'Мужские Кроссовки Nike Blazer Mid Suede', price: 12999, imageUrl:'/img/sneakers/1.jpg' },
-	{title: 'Мужские Кроссовки Nike Air Max 270', price: 12999, imageUrl:'/img/sneakers/2.jpg' },
-	{title: 'Мужские Кроссовки Nike Blazer Mid Suede', price: 8499, imageUrl:'/img/sneakers/3.jpg' },
-	{title: 'Кроссовки Puma X Aka Boku Future Rider', price: 8999, imageUrl:'/img/sneakers/4.jpg' }
-]
 
 function App() {
+	const [items, setItems] = React.useState([]);
+	const [cartItems, setCartItems] = React.useState([]);
+	const [cartOpened, setCartOpened] = React.useState(false);
+
+	React.useEffect(() => {
+		fetch('https://64c26ce7eb7fd5d6ebcfd745.mockapi.io/items').then(res => {
+			return res.json();
+		}).then(json => {
+			setItems(json);
+		});
+	}, [])
+
+	const onAddToCart = (obj) => {
+		setCartItems((prev) => [...prev, obj]);
+	}
+
+	const onRemoveToCart = (obj) => {
+		setCartItems([(prev) => [...prev, obj.delete]]);
+	}
+
 	return (
 		<div className="wrapper clear">
-			<Drawer />
-			<Header />
+			{cartOpened && <Drawer items={cartItems} onRemove={(obj) => onRemoveToCart(obj)} onClose={() => setCartOpened(false)} />}
+			<Header onClickCart={() => setCartOpened(true)} />
 			<div className="content p-40">
 				<div className="d-flex align-center mb-40 justify-between">
 					<h1>Все кроссовки</h1>
@@ -23,10 +38,16 @@ function App() {
 					</div>
 				</div>
 
-				<div className="d-flex">
+				<div className="d-flex flex-wrap">
 					{
-						arr.map((obj) => (
-							<Card title={obj.title} price={obj.price} imageUrl={obj.imageUrl} onClick={() => console.log(obj)}/>
+						items.map((item) => (
+							<Card
+								title={item.title}
+								price={item.price}
+								imageUrl={item.imageUrl}
+								onClickFavorite={() => console.log('Добавили в закладки')}
+								onPlus={(obj) => onAddToCart(obj)}
+							/>
 						))}
 				</div>
 			</div>
--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,23 +1,28 @@
+import React from "react";
 import styles from './Card.module.scss'
 
-console.log(styles)
+function Card({ title, imageUrl, price, onClickFavorite, onPlus }) {
+	const [isAdded, setIsAdded] = React.useState(false);
 
-function Card(props) {
+
+	const onClickPlus = () => {
+		onPlus({ title, imageUrl, price });
+		setIsAdded(!isAdded);
+	}
+
 	return (
 		<div className={styles.card}>
-			<div className={styles.favorite}>
+			<div className={styles.favorite} onClick={onClickFavorite}>
 				<img src="/img/heart-unliked.svg" alt="Unliked" />
 			</div>
-			<img width={133} height={112} src={props.imageUrl} alt="Sneakers" />
-			<h5>{props.title}</h5>
+			<img width={133} height={112} src={imageUrl} alt="Sneakers" />
+			<h5>{title}</h5>
 			<div className="d-flex justify-between align-center">
 				<div className="d-flex flex-column">
 					<span>Цена:</span>
-					<b>{props.price} руб.</b>
+					<b>{price} руб.</b>
 				</div>
-				<button className="button" onClick={props.onClick}>
-					<img width={11} height={11} src="/img/add.svg" alt="add" />
-				</button>
+				<img className={styles.plus} onClick={onClickPlus} src={isAdded ? "/img/btn-checked.svg" : "/img/plus.svg"} alt="add" />
 			</div>
 		</div>
 
